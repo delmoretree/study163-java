5f85235904fd77d71853ef5ccc895ead1ef5aeda
mac <zyq1516840114@163.com>
2020-11-23 20:06:07 +0800

    modify

---
+++ chapter2/src/test/java/tech/technote/delmore/DelmoreApplicationTests.java
@@ -0,0 +1,11 @@
+package tech.technote.delmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DelmoreApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
